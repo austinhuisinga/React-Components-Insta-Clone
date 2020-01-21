5240d2e0d059db4e8d457547d1ad433788aa1534
austinhuisinga <57517058+austinhuisinga@users.noreply.github.com>
2020-01-21 16:33:51 -0700

    MVP Complete.

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -6,17 +6,16 @@ const LikeSection = props => {
     <div
       className="like-section"
       key="likes-icons-container"
+      onClick={props.changeLike}
     >
       <div className="like-section-wrapper">
-        <i className="far fa-heart" />
+        <i className="far fa-heart"  />
       </div>
       <div className="like-section-wrapper">
         <i className="far fa-comment" />
       </div>
     </div>
-    <p className="like-number">
-      
-      likes</p>
+    <p className="like-number">{props.likes} likes</p>
 </div>
   )
 };
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -3,11 +3,13 @@ import React from "react";
 import Post from "./Post";
 import "./Posts.css";
 
+
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+const PostsPage = (props) => {
   return (
     <div className="posts-container-wrapper">
       {/* map through data here */}
+      {props.postData.map((item) => <Post post={item} key={item.imageUrl} />)}
     </div>
   );
 };
